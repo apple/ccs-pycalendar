adf010708c5bb9e0ebaddd44eb538991cccad0ca
Cyrus Daboo <cdaboo@apple.com>
2007-12-21 04:51:44 +0000

    Cosmetic.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/PyCalendar/trunk@10409 e27351fd-9f3e-4f54-a53b-843176b1656c

--- src/pycalendar/utils.py
+++ src/pycalendar/utils.py
@@ -184,8 +184,8 @@ def leapDaysSince1970(year_offset):
     if year_offset > 2:
         return (year_offset + 1) / 4
     elif year_offset < -1:
-         # Python will round down negative numbers (i.e. -5/4 = -2, but we want -1), so
-         # what is (year_offset - 2) in C code is actually (year_offset - 2 + 3) in Python.
+        # Python will round down negative numbers (i.e. -5/4 = -2, but we want -1), so
+        # what is (year_offset - 2) in C code is actually (year_offset - 2 + 3) in Python.
         return (year_offset + 1) / 4
     else:
         return 0
