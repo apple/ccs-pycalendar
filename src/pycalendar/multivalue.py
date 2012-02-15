f3f84966e3bd861b77a6b40a42239779897256f6
Cyrus Daboo <cdaboo@apple.com>
2012-02-15 03:59:24 +0000

    Fix multi-value setValue.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/PyCalendar/branches/server@10519 e27351fd-9f3e-4f54-a53b-843176b1656c

--- src/pycalendar/multivalue.py
+++ src/pycalendar/multivalue.py
@@ -1,5 +1,5 @@
 ##
-#    Copyright (c) 2007-2011 Cyrus Daboo. All rights reserved.
+#    Copyright (c) 2007-2012 Cyrus Daboo. All rights reserved.
 #    
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -47,7 +47,12 @@ class PyCalendarMultiValue( PyCalendarValue ):
         self.mValues.append( value )
 
     def setValue(self, value):
-        self.mValues = [PyCalendarValue.createFromType(self.mType).setValue(v) for v in value]
+        newValues = []
+        for v in value:
+            pyCalendarValue = PyCalendarValue.createFromType(self.mType)
+            pyCalendarValue.setValue(v)
+            newValues.append(pyCalendarValue)
+        self.mValues = newValues
 
     def parse( self, data ):
         # Tokenize on comma
---
+++ src/pycalendar/tests/test_multivalue.py
@@ -0,0 +1,44 @@
+##
+#    Copyright (c) 2011-2012 Cyrus Daboo. All rights reserved.
+#    
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#    
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##
+
+import unittest
+from pycalendar.multivalue import PyCalendarMultiValue
+from pycalendar.value import PyCalendarValue
+
+class TestMultiValue(unittest.TestCase):
+    
+    def testParseValue(self):
+        
+        items = (
+            ("", "", 1),
+            ("Example", "Example", 1),
+            ("Example1,Example2", "Example1,Example2", 2),
+        )
+        
+        for item, result, count in items:
+            req = PyCalendarMultiValue(PyCalendarValue.VALUETYPE_TEXT)
+            req.parse(item)
+            test = req.getText()
+            self.assertEqual(test, result, "Failed to parse and re-generate '%s'" % (item,))
+            self.assertEqual(len(req.mValues), count, "Failed to parse and re-generate '%s'" % (item,))
+
+    def testSetValue(self):
+        
+        req = PyCalendarMultiValue(PyCalendarValue.VALUETYPE_TEXT)
+        req.parse("Example1, Example2")
+        req.setValue(("Example3", "Example4",))
+        test = req.getText()
+        self.assertEqual(test, "Example3,Example4")
