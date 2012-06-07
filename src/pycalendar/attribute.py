f98036b1614c546aa24201fca6a2d576b7885719
Cyrus Daboo <cdaboo@apple.com>
2012-06-07 22:45:35 +0000

    Remove redundant copy.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/PyCalendar/branches/server@10529 e27351fd-9f3e-4f54-a53b-843176b1656c

--- src/pycalendar/attribute.py
+++ src/pycalendar/attribute.py
@@ -1,5 +1,5 @@
 ##
-#    Copyright (c) 2007-2011 Cyrus Daboo. All rights reserved.
+#    Copyright (c) 2007-2012 Cyrus Daboo. All rights reserved.
 #    
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ class PyCalendarAttribute(object):
             self.mValues = value
 
     def duplicate(self):
-        other = PyCalendarAttribute(self.mName, [i for i in self.mValues])
+        other = PyCalendarAttribute(self.mName)
         other.mValues = self.mValues[:]
         return other
 
