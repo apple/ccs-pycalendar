35f5e614c22b1a9236ed5cd492e31b03c080eeb8
Cyrus Daboo <cdaboo@apple.com>
2015-01-06 16:20:22 +0000

    Addition recurrence-related api.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/PyCalendar/trunk@14245 e27351fd-9f3e-4f54-a53b-843176b1656c

--- src/pycalendar/icalendar/calendar.py
+++ src/pycalendar/icalendar/calendar.py
@@ -351,6 +351,80 @@ class Calendar(ContainerBase):
                 del self.mMasterComponentsByTypeAndUID[component.getType()][uid]
 
 
+    def deriveComponent(self, recurrenceID):
+        """
+        Derive an overridden component for the associated RECURRENCE-ID. This assumes
+        that the R-ID is valid for the actual recurrence being used.
+
+        @param recurrenceID: the recurrence instance
+        @type recurrenceID: L{DateTime}
+
+        @return: the derived component
+        @rtype: L{ComponentRecur} or L{None}
+        """
+        master = self.masterComponent()
+        if master is None:
+            return None
+
+        # Create the derived instance
+        newcomp = master.duplicate()
+
+        # Strip out unwanted recurrence properties
+        for propname in (
+            definitions.cICalProperty_RRULE,
+            definitions.cICalProperty_RDATE,
+            definitions.cICalProperty_EXRULE,
+            definitions.cICalProperty_EXDATE,
+            definitions.cICalProperty_RECURRENCE_ID,
+        ):
+            newcomp.removeProperties(propname)
+
+        # New DTSTART is the RECURRENCE-ID we are deriving but adjusted to the
+        # original DTSTART's localtime
+        dtstart = newcomp.getStart()
+        dtend = newcomp.getEnd()
+        oldduration = dtend - dtstart
+
+        newdtstartValue = recurrenceID.duplicate()
+        if not dtstart.isDateOnly():
+            if dtstart.local():
+                newdtstartValue.adjustTimezone(dtstart.getTimezone())
+        else:
+            newdtstartValue.setDateOnly(True)
+
+        newcomp.removeProperties(definitions.cICalProperty_DTSTART)
+        newcomp.removeProperties(definitions.cICalProperty_DTEND)
+        prop = Property(definitions.cICalProperty_DTSTART, newdtstartValue)
+        newcomp.addProperty(prop)
+        if not newcomp.useDuration():
+            prop = Property(definitions.cICalProperty_DTEND, newdtstartValue + oldduration)
+            newcomp.addProperty(prop)
+
+        newcomp.addProperty(Property("RECURRENCE-ID", newdtstartValue))
+
+        # After creating/changing a component we need to do this to keep PyCalendar happy
+        newcomp.finalise()
+
+        return newcomp
+
+
+    def masterComponent(self):
+        """
+        Return the first sub-component of a recurring type that represents the master
+        instance.
+
+        @return: the master component
+        @rtype: L{ComponentRecur} or L{None}
+        """
+        for component in self.getComponents():
+            if isinstance(component, ComponentRecur):
+                rid = component.getRecurrenceID()
+                if rid is None:
+                    return component
+        else:
+            return None
+
+
     def getText(self, includeTimezones=None, format=None):
 
         if format is None or format == self.sFormatText:
