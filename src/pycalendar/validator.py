afb6ef17738916f75383bec3c4f8002f5c4c8e75
Cyrus Daboo <cdaboo@apple.com>
2016-08-18 11:02:42 -0400

    More descriptive error messages.

--- src/pycalendar/validator.py
+++ src/pycalendar/validator.py
@@ -36,30 +36,30 @@ def validate(fname):
         try:
             cal = Calendar.parseText(data)
         except ErrorBase as e:
-            print "Failed to parse iCalendar: %r" % (e,)
+            print("Failed to parse iCalendar: {}: {}".format(e.mReason, e.mData,))
             sys.exit(1)
     elif data.find("BEGIN:VCARD") != -1:
         try:
             cal = Card.parseText(data)
         except ErrorBase as e:
-            print "Failed to parse vCard: %r" % (e,)
+            print("Failed to parse vCard: {}: {}".format(e.mReason, e.mData,))
             sys.exit(1)
     else:
-        print "Failed to find valid iCalendar or vCard data"
+        print("Failed to find valid iCalendar or vCard data")
         sys.exit(1)
 
     _ignore_fixed, unfixed = cal.validate(doFix=False, doRaise=False)
     if unfixed:
-        print "List of problems: %s" % (unfixed,)
+        print("List of problems: {}".format(unfixed,))
     else:
-        print "No problems"
+        print("No problems")
 
     # Control character check - only HTAB, CR, LF allowed for characters in the range 0x00-0x1F
     s = str(data)
     if len(s.translate(None, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F")) != len(s):
         for ctr, i in enumerate(data):
             if i in "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F":
-                print "Control character %d at position %d" % (ord(i), ctr,)
+                print("Control character {} at position {}".format(ord(i), ctr,))
 
 
 if __name__ == '__main__':
